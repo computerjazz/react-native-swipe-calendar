6fd13847ec4a05fa1ef3f737fd2488e6b1fe7f47
computerjazz <hi@danielmerrill.com>
2021-10-18 12:30:30 -0700

    perf upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import React, {
   useRef,
   useEffect,
   useImperativeHandle,
+  useCallback,
 } from "react";
 import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 import Animated, {
@@ -21,6 +22,7 @@ import {
   isSameDay,
   differenceInCalendarMonths,
 } from "date-fns";
+import { isEqual } from "lodash";
 import InfinitePager, {
   InfinitePagerImperativeApi,
 } from "react-native-infinite-pager";
@@ -29,13 +31,13 @@ const DEFAULT_THEME = {
   textActiveColor: "black",
   textInactiveColor: "grey",
   todayIndicatorDotColor: "tomato",
-  slectedDayBackgroundColor: "rgba(0, 0, 255, 0.25)",
+  selectedDayBackgroundColor: "rgba(0, 0, 255, 0.25)",
   fontFamily: "sans-serif",
   dayFontSize: 12,
   dayLabelFormat: "EEEEEE",
   headerDateFormat: "MMMM yyyy",
   headerFontSize: 24,
-  inactiveOpacity: 0,
+  inactiveOpacity: 1,
 };
 
 type OnDateSelect =
@@ -43,7 +45,7 @@ type OnDateSelect =
   | ((date: Date, options: { isSelected: boolean }) => void);
 type DayComponentType = (props: {
   date: Date;
-  isInActiveMonth: boolean;
+  isInDisplayedMonth: boolean;
   isSelected: boolean;
   isToday: boolean;
 }) => JSX.Element | null;
@@ -61,133 +63,222 @@ export function useCalendarContext() {
   return useContext(CalendarContext);
 }
 
-export function MonthPage({
-  index,
-  focusAnim,
-}: {
-  index: number;
-  focusAnim: Animated.DerivedValue<number>;
-}) {
-  const { referenceDate, TitleComponent, theme } = useCalendarContext();
-  const firstDayOfMonth = addMonths(referenceDate, index);
-  firstDayOfMonth.setDate(1);
-  const lastDayOfMo = lastDayOfMonth(firstDayOfMonth);
-  const title = format(firstDayOfMonth, theme.headerDateFormat);
-  const weekStarts = eachWeekOfInterval({
-    start: firstDayOfMonth,
-    end: lastDayOfMo,
-  });
+export const MonthPage = React.memo(
+  ({
+    index,
+    focusAnim,
+  }: {
+    index: number;
+    focusAnim: Animated.DerivedValue<number>;
+  }) => {
+    const { referenceDate, TitleComponent, theme } = useCalendarContext();
+    const firstDayOfMonth = useMemo(
+      () => addMonths(referenceDate, index),
+      [referenceDate, index]
+    );
+    firstDayOfMonth.setDate(1);
+    const lastDayOfMo = useMemo(
+      () => lastDayOfMonth(firstDayOfMonth),
+      [firstDayOfMonth]
+    );
+    const title = format(firstDayOfMonth, theme.headerDateFormat);
+    const weekStarts = useMemo(
+      () =>
+        eachWeekOfInterval({
+          start: firstDayOfMonth,
+          end: lastDayOfMo,
+        }),
+      [firstDayOfMonth, lastDayOfMo]
+    );
 
-  const weeks = weekStarts.map((week) => {
-    return eachDayOfInterval({ start: week, end: addDays(week, 6) });
-  });
+    const weeks = useMemo(
+      () =>
+        weekStarts.map((week) => {
+          return eachDayOfInterval({ start: week, end: addDays(week, 6) });
+        }),
+      [weekStarts]
+    );
 
-  const animStyle = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(
-        focusAnim.value,
-        [0, 1],
-        [theme.inactiveOpacity, 1],
-        Animated.Extrapolate.CLAMP
-      ),
-    };
-  }, []);
-
-  return (
-    <Animated.View style={[animStyle, { alignItems: "center" }]}>
-      {TitleComponent ? (
-        <TitleComponent date={firstDayOfMonth} />
-      ) : (
-        <Text
-          style={{
-            fontSize: theme.headerFontSize,
-            color: theme.textActiveColor,
-          }}
-        >
-          {title}
-        </Text>
-      )}
-      <View style={styles.row}>
-        <View style={styles.weeksContainer}>
-          {weeks[0].map((day) => {
-            const dayLabel = format(day, theme.dayLabelFormat);
-            return (
-              <View style={styles.dayLabelContainer}>
-                <Text style={{ color: theme.textActiveColor }}>{dayLabel}</Text>
-              </View>
+    const animStyle = useAnimatedStyle(() => {
+      const opacity =
+        theme.inactiveOpacity === 1
+          ? 1
+          : interpolate(
+              focusAnim.value,
+              [0, 1],
+              [theme.inactiveOpacity, 1],
+              Animated.Extrapolate.CLAMP
             );
-          })}
+      return {
+        opacity,
+      };
+    }, [focusAnim]);
+    const thisMonth = useMemo(
+      () => format(firstDayOfMonth, "MM"),
+      [firstDayOfMonth]
+    );
+    return (
+      <Animated.View style={[animStyle, { alignItems: "center" }]}>
+        {TitleComponent ? (
+          <TitleComponent date={firstDayOfMonth} />
+        ) : (
+          <Text
+            style={{
+              fontSize: theme.headerFontSize,
+              color: theme.textActiveColor,
+            }}
+          >
+            {title}
+          </Text>
+        )}
+        <View style={styles.row}>
+          <View style={styles.dayLabelRow}>
+            {weeks[0].map((day) => {
+              const dayLabel = format(day, theme.dayLabelFormat);
+              return (
+                <View style={styles.dayLabelContainer}>
+                  <Text style={{ color: theme.textActiveColor }}>
+                    {dayLabel}
+                  </Text>
+                </View>
+              );
+            })}
+          </View>
         </View>
-      </View>
-      <Animated.View style={styles.row}>
-        <Animated.View style={{ flex: 1 }}>
-          {weeks.map((daysInWeek) => {
-            return (
-              <Animated.View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                {daysInWeek.map((day) => {
-                  const sameMonth = isSameMonth(day, firstDayOfMonth);
-                  return <DayItem sameMonth={sameMonth} date={day} />;
-                })}
-              </Animated.View>
-            );
-          })}
+        <Animated.View style={styles.row}>
+          <Animated.View style={{ flex: 1 }}>
+            {weeks.map((daysInWeek) => {
+              return (
+                <Animated.View style={styles.weekContainer}>
+                  {daysInWeek.map((day) => {
+                    const sameMonth = isSameMonth(day, firstDayOfMonth);
+                    const dayMonth = format(day, "MM-dd");
+                    const key = `day-${dayMonth}-thisMonth:${thisMonth}`;
+                    return (
+                      <DayWrapper
+                        key={key}
+                        isInDisplayedMonth={sameMonth}
+                        date={day}
+                      />
+                    );
+                  })}
+                </Animated.View>
+              );
+            })}
+          </Animated.View>
         </Animated.View>
       </Animated.View>
-    </Animated.View>
-  );
-}
+    );
+  }
+);
 
-function DayItem({ date, sameMonth }: { date: Date; sameMonth: boolean }) {
-  const { selectedDate, onDateSelect, DayComponent, theme } =
-    useCalendarContext();
-  const dayText = format(date, "d");
-  const color = sameMonth ? theme.textActiveColor : theme.textInactiveColor;
-  const isSelected = !!selectedDate && isSameDay(date, selectedDate);
-  const isToday = isSameDay(date, new Date());
-  if (DayComponent) {
+type DayWrapperProps = {
+  isInDisplayedMonth: boolean;
+  date: Date;
+};
+
+const DayWrapper = React.memo(
+  ({ date, isInDisplayedMonth }: DayWrapperProps) => {
+    const dateRef = useRef(date);
+    const memoDate = useMemo(() => {
+      if (isSameDay(dateRef.current, date)) {
+        return dateRef.current;
+      } else {
+        dateRef.current = date;
+        return date;
+      }
+    }, [date]);
+
+    const { selectedDate, onDateSelect, DayComponent, theme } =
+      useCalendarContext();
+    const isSelected = useMemo(() => {
+      return !!selectedDate && isSameDay(memoDate, selectedDate);
+    }, [memoDate, selectedDate]);
+    const isToday = useMemo(() => isSameDay(memoDate, new Date()), [memoDate]);
+    const onDateSelectRef = useRef(onDateSelect);
+    onDateSelectRef.current = onDateSelect;
+    const onDateSelectCb: OnDateSelect = useCallback((date, options) => {
+      return onDateSelectRef.current?.(date, options);
+    }, []);
+
     return (
-      <DayComponent
-        date={date}
-        isInActiveMonth={sameMonth}
+      <DayItem
+        date={memoDate}
         isSelected={isSelected}
         isToday={isToday}
+        isInDisplayedMonth={isInDisplayedMonth}
+        DayComponent={DayComponent}
+        onDateSelect={onDateSelectCb}
+        theme={theme}
       />
     );
   }
+);
 
-  return (
-    <TouchableOpacity
-      onPress={() => onDateSelect?.(date, { isSelected })}
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 5,
-        backgroundColor: isSelected
-          ? theme.slectedDayBackgroundColor
-          : "transparent",
-        borderRadius: 5,
-      }}
-    >
-      <Text style={{ color: color }}>{dayText}</Text>
-      <View
+type DayProps = {
+  date: Date;
+  isInDisplayedMonth: boolean;
+  isSelected: boolean;
+  isToday: boolean;
+  DayComponent?: DayComponentType;
+  onDateSelect?: OnDateSelect;
+  theme: typeof DEFAULT_THEME;
+};
+
+const DayItem = React.memo(
+  ({
+    date,
+    isInDisplayedMonth,
+    isSelected,
+    DayComponent,
+    isToday,
+    onDateSelect,
+    theme,
+  }: DayProps) => {
+    const dayText = format(date, "d");
+    const color = isInDisplayedMonth
+      ? theme.textActiveColor
+      : theme.textInactiveColor;
+    if (DayComponent) {
+      return (
+        <DayComponent
+          date={date}
+          isInDisplayedMonth={isInDisplayedMonth}
+          isSelected={isSelected}
+          isToday={isToday}
+        />
+      );
+    }
+
+    return (
+      <TouchableOpacity
+        onPress={() => onDateSelect?.(date, { isSelected })}
         style={{
-          width: 5,
-          height: 5,
-          borderRadius: 5,
-          backgroundColor: isToday
-            ? theme.todayIndicatorDotColor
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 5,
+          backgroundColor: isSelected
+            ? theme.selectedDayBackgroundColor
             : "transparent",
+          borderRadius: 5,
         }}
-      />
-    </TouchableOpacity>
-  );
-}
+      >
+        <Text style={{ color }}>{dayText}</Text>
+        <View
+          style={{
+            width: 5,
+            height: 5,
+            borderRadius: 5,
+            backgroundColor: isToday
+              ? theme.todayIndicatorDotColor
+              : "transparent",
+          }}
+        />
+      </TouchableOpacity>
+    );
+  }
+);
 
 export type CalendarImperativeApi = {
   incrementMonth: () => void;
@@ -197,15 +288,10 @@ export type CalendarImperativeApi = {
 
 type CalendarProps = {
   selectedDate?: Date | null;
-  onDateSelect?: (date: Date) => void;
+  onDateSelect?: OnDateSelect;
   onMonthChange?: (date: Date) => void;
   currentDate?: Date;
-  DayComponent?: (props: {
-    date: Date;
-    isInActiveMonth: boolean;
-    isSelected: boolean;
-    isToday: boolean;
-  }) => JSX.Element | null;
+  DayComponent?: DayComponentType;
   TitleComponent?: (props: { date: Date }) => JSX.Element | null;
   theme?: Partial<typeof DEFAULT_THEME>;
   monthBuffer?: number;
@@ -229,23 +315,42 @@ function Calendar(
   const currentDateRef = useRef(currentDate);
   const currentPageRef = useRef(0);
 
-  const fullTheme = {
+  const onMonthChangeRef = useRef(onMonthChange);
+  onMonthChangeRef.current = onMonthChange;
+
+  const fullThemeObj = {
     ...DEFAULT_THEME,
     ...theme,
   };
+  const fullThemeRef = useRef(fullThemeObj);
 
-  useImperativeHandle(ref, () => ({
-    incrementMonth: () => {
-      pagerRef.current?.incrementPage({ animated: true });
-    },
-    decrementMonth: () => {
-      pagerRef.current?.decrementPage({ animated: true });
-    },
-    setMonth: (date: Date) => {
-      const page = differenceInCalendarMonths(date, initialDate);
-      pagerRef.current?.setPage(page, { animated: false });
-    },
-  }));
+  const fullTheme: typeof DEFAULT_THEME = useMemo(() => {
+    const updatedTheme = { ...DEFAULT_THEME, ...theme };
+
+    if (isEqual(fullThemeRef.current, updatedTheme)) {
+      return fullThemeRef.current;
+    } else {
+      fullThemeRef.current = updatedTheme;
+      return updatedTheme;
+    }
+  }, [theme]);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      incrementMonth: () => {
+        pagerRef.current?.incrementPage({ animated: true });
+      },
+      decrementMonth: () => {
+        pagerRef.current?.decrementPage({ animated: true });
+      },
+      setMonth: (date: Date) => {
+        const page = differenceInCalendarMonths(date, initialDate);
+        pagerRef.current?.setPage(page, { animated: false });
+      },
+    }),
+    []
+  );
 
   useEffect(() => {
     if (
@@ -261,42 +366,62 @@ function Calendar(
     currentDateRef.current = currentDate;
   }, [currentDate, initialDate]);
 
+  const onPageChange = useCallback(
+    (pg: number) => {
+      currentPageRef.current = pg;
+      const currentMonth = addMonths(initialDate, pg);
+      currentMonth.setDate(1);
+      onMonthChangeRef.current?.(currentMonth);
+    },
+    [initialDate]
+  );
+
+  const providerValue = useMemo(
+    () => ({
+      referenceDate: initialDate,
+      selectedDate,
+      onDateSelect,
+      DayComponent,
+      TitleComponent,
+      theme: fullTheme,
+    }),
+    [
+      initialDate,
+      selectedDate,
+      onDateSelect,
+      DayComponent,
+      TitleComponent,
+      fullTheme,
+    ]
+  );
+
   return (
-    <CalendarContext.Provider
-      value={{
-        referenceDate: initialDate,
-        selectedDate,
-        onDateSelect,
-        DayComponent,
-        TitleComponent,
-        theme: fullTheme,
-      }}
-    >
+    <CalendarContext.Provider value={providerValue}>
       <InfinitePager
         ref={pagerRef}
         PageComponent={MonthPage}
         pageBuffer={monthBuffer}
-        onPageChange={(pg) => {
-          currentPageRef.current = pg;
-          const currentMonth = addMonths(initialDate, pg);
-          currentMonth.setDate(1);
-          onMonthChange?.(currentMonth);
-        }}
+        onPageChange={onPageChange}
       />
     </CalendarContext.Provider>
   );
 }
 
-export default React.forwardRef(Calendar);
+export default React.memo(React.forwardRef(Calendar));
 
 const styles = StyleSheet.create({
+  flex: { flex: 1 },
   row: { flexDirection: "row" },
   dayLabelContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  weeksContainer: {
+  weekContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  dayLabelRow: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
